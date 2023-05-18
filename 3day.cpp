1718a9b57b1f3cadd00980d7c971bc20ff5929aa
Siddharth Singh <singhsiddharth1102@gmail.com>
2023-05-18 14:58:17 +0530

    complete 3 star in c++

---
+++ 3day.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseArry(int arr[],int n){
+    int start =0;
+    int end = n-1;
+    while(start<=end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+   
+       reverseArry(arr,n);
+        for(int i =0;i<n;i++){
+            cout<< arr[i]<<" ";
+        }
+        cout<<endl;
+
+    return 0;
+}
